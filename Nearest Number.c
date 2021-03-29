34dcd7f12956334a9b1f5e61f4c327ae168b1f6e
Paphatsinee <paphatsinee.r@ku.th>
2021-03-29 20:04:35 +0700

    Update Nearest Number.c

--- Nearest Number.c
+++ Nearest Number.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,m;
-    char a[10];
-    scanf("%s %c",a,&n);
-    m=a;
-    for(int i=0;i<10;i++)
-    {
-        while(a[i]!=n)
-        {
-            a[i]+=1;
-            m++;
-        }
-    }
-    printf("%s",a);
+    int n,m,r=0,i,a;
+    scanf("%d %d",&n,&m);
 
+    for(i=0;r!=m;i++)
+    {
+        a=n;
+        while(a>=10)
+        {
+            a=a/10;r=a%10;
+
+        }
+        if(r!=m)
+        {
+            n++;
+        }
+        else
+            break;
+
+    }
+    printf("%d",r);
+return 0;
 }
